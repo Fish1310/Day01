46cc39d0adab70634fedb851265c701df7be82d2
Antoine Chopard <antoinechopard@MacBook-Pro-de-Antoine.local>
2024-09-30 12:14:24 +0200

    Finish ex5

---
+++ ex_05.js
@@ -0,0 +1,13 @@
+let firstname = "John";
+let lastname = "Doe";
+let cardCode = "1111 2222 3333 4444";
+let expiryDateMonth = 21;
+let expiryDateYear = 2042;
+
+let computedBankInfo = firstname + "," + lastname + "," + cardCode + "," + expiryDateMonth + "/" + expiryDateYear;
+
+function displayInCard(info) {
+    document.body.innerHTML = `<p>${info}</p>`;
+}
+
+displayInCard(computedBankInfo);
