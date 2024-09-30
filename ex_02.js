1dd1ea1399ae5506decb53ba6515574bc86c9229
Antoine Chopard <antoinechopard@MacBook-Pro-de-Antoine.local>
2024-09-30 11:48:55 +0200

    Finish ex02

---
+++ ex_02.js
@@ -0,0 +1,14 @@
+let bool = true;             
+let string = "forty two";     
+let integer = 42;             
+let float = 42.42;            
+let this_is_null = null;      
+
+
+document.body.innerHTML = `
+    <p>bool: ${bool}</p>
+    <p>string: ${string}</p>
+    <p>integer: ${integer}</p>
+    <p>float: ${float}</p>
+    <p>this_is_null: ${this_is_null}</p>
+`;
