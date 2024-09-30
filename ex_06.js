612013c1bad67479a8a816e5d072a3eb6e765c9e
Antoine Chopard <antoinechopard@MacBook-Pro-de-Antoine.local>
2024-09-30 12:27:47 +0200

    Finish ex6

---
+++ ex_06.js
@@ -0,0 +1,10 @@
+// let bananasCount = -9;
+// let bananasCount = 15;
+
+if (bananasCount === 0) {
+    document.body.innerHTML = "Oh no, there is no banana.";
+} else if (bananasCount > 0) {
+    document.body.innerHTML = "Yummy!";
+} else if (bananasCount < 0) {
+    document.body.innerHTML = "Do I owe you bananas?";
+}
