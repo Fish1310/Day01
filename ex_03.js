63350e195396605104006f9b42c31ef48ad9af44
Antoine Chopard <antoinechopard@MacBook-Pro-de-Antoine.local>
2024-09-30 12:01:02 +0200

    Finish03

---
+++ ex_03.js
@@ -0,0 +1,7 @@
+
+let number1 = "1"; 
+let number2 = "2";   
+
+let result = number1 + number2; 
+
+document.body.innerHTML = `Le résultat de la combinaison de ${number1} et ${number2} est : ${result}`;
